2e6bc70c0265aa0011466bcb9de41f714d4f2b4f
richard <RichardWoodhead00@gmail.com>
2021-04-14 20:11:59 +0000

    DM

---
+++ Assets/Scripts/DeathMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour
+{
+    public void RestartGame(){
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
